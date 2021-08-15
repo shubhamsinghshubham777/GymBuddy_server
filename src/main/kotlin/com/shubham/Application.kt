e8be616f534eccc72e063ba67cea0763dc78463a
shubham <shubhamsinghshubham777@gmail.com>
2021-08-15 23:28:25 +0530

    tweaked env variables

--- src/main/kotlin/com/shubham/Application.kt
+++ src/main/kotlin/com/shubham/Application.kt
@@ -3,9 +3,13 @@ package com.shubham
 import io.ktor.server.engine.*
 import io.ktor.server.cio.*
 import com.shubham.plugins.*
+import java.net.URI
 
 fun main() {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
+
+    val uri = URI(System.getenv("DATABASE_URL"))
+
+    embeddedServer(CIO, port = System.getenv("PORT").toInt(), host = uri.host) {
         configureRouting()
         configureMonitoring()
     }.start(wait = true)
