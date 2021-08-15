4e3fc25be2b1e55af5d66cc3882d09570d4557b1
shubham <shubhamsinghshubham777@gmail.com>
2021-08-15 22:27:27 +0530

    sslmode allow

--- src/main/kotlin/com/shubham/database/DatabaseManager.kt
+++ src/main/kotlin/com/shubham/database/DatabaseManager.kt
@@ -17,7 +17,7 @@ class DatabaseManager {
 
     init {
 //        val jdbcUrl = "jdbc:postgresql://$hostname:5432/$databaseName?user=$username&password=$password&useSSL=false"
-        val jdbcUrl = "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}?user=$username&password=$password&sslmode=disable"
+        val jdbcUrl = "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}?user=$username&password=$password&sslmode=allow"
         println("You are connecting to this url: $jdbcUrl")
         ktormDatabase = Database.connect(jdbcUrl)
     }
