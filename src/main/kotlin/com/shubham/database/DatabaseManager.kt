10c507a60c867a5c6cbf1609ca3799ff18e0dc49
shubham <shubhamsinghshubham777@gmail.com>
2021-08-19 11:49:29 +0530

    Changed url to heroku's url inside DatabaseManager.kt

--- src/main/kotlin/com/shubham/database/DatabaseManager.kt
+++ src/main/kotlin/com/shubham/database/DatabaseManager.kt
@@ -6,21 +6,21 @@ import java.net.URI
 class DatabaseManager {
 
     /** Local Database Credentials */
-    private val hostname = "localhost"
-    private val databaseName = "gymbuddy"
-    private val username = System.getenv("DB_USERNAME")
-    private val password = System.getenv("DB_PASSWORD")
+//    private val hostname = "localhost"
+//    private val databaseName = "gymbuddy"
+//    private val username = System.getenv("DB_USERNAME")
+//    private val password = System.getenv("DB_PASSWORD")
 
     /** Heroku DB Credentials */
-//    private val uri = URI(System.getenv("DATABASE_URL"))
-//    private val username = uri.userInfo.split(":").toTypedArray()[0]
-//    private val password = uri.userInfo.split(":").toTypedArray()[1]
+    private val uri = URI(System.getenv("DATABASE_URL"))
+    private val username = uri.userInfo.split(":").toTypedArray()[0]
+    private val password = uri.userInfo.split(":").toTypedArray()[1]
 
     val ktormDatabase: Database
 
     init {
-        val jdbcUrl = "jdbc:postgresql://$hostname:5432/$databaseName?user=$username&password=$password&useSSL=false"
-//        val jdbcUrl = "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}?user=$username&password=$password&sslmode=allow"
+//        val jdbcUrl = "jdbc:postgresql://$hostname:5432/$databaseName?user=$username&password=$password&useSSL=false"
+        val jdbcUrl = "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}?user=$username&password=$password&sslmode=allow"
         println("You are connecting to this url: $jdbcUrl")
         ktormDatabase = Database.connect(jdbcUrl)
     }
